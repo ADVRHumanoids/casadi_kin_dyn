5b124d6aabdd2c59f203c7d8749f76cd732e1071
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-12-13 13:25:17 +0100

    plat-specific pkg

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
 import setuptools
-import subprocess
 
 setuptools.setup(
     name="casadi_kin_dyn",
     version="1.0.0",
-    author="Francesco Ruscelli",
-    author_email="francesco.ruscelli@iit.it",
-    description="Library for Trajectory Optimization based on CasADi",
-    # long_description=long_description,
+    author="Arturo Laurenzi",
+    author_email="arturo.laurenzi@iit.it",
+    description="Package for generation of symbolic (casadi::SX) expressions of robot kinematics and dynamics. Based on URDF and Pinocchio.",
     long_description_content_type="text/markdown",
     url="https://github.com/FrancescoRuscelli/horizon_gui",
-    # project_urls={
-    #     "Bug Tracker": "no",
-    # },
-    # classifiers=[
-    #     "Programming Language :: Python :: 3",
-    #     "Operating System :: Ubuntu",
-    # ],
-    # package_dir={"": "horizon"},
     packages=['casadi_kin_dyn'],
     install_requires=[],
-    # packages=setuptools.find_packages(),
-    python_requires=">=3.6"
-    
+    python_requires=">=3.6",
+    ext_modules=[
+        setuptools.Extension(
+            name='pycasadi_kin_din',
+            sources=[]
+        )
+    ]
 )
