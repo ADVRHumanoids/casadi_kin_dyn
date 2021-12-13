24b49908046193ce1b93700f108dd0638729a33a
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-12-13 17:12:36 +0100

    url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     author_email="arturo.laurenzi@iit.it",
     description="Package for generation of symbolic (casadi::SX) expressions of robot kinematics and dynamics. Based on URDF and Pinocchio.",
     long_description_content_type="text/markdown",
-    url="https://github.com/FrancescoRuscelli/horizon_gui",
+    url="https://github.com/ADVRHumanoids/casadi_kin_dyn",
     packages=['casadi_kin_dyn'],
     install_requires=[],
     python_requires=">=3.6",
