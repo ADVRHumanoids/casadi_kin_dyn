5b9d0fd223b383ee436246bd6bc36490431571da
enricomingo <enrico.mingo@pal-robotics.com>
2022-04-27 11:19:37 +0200

    new version 1.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="casadi_kin_dyn",
-    version="1.3.1",
+    version="1.3.2",
     author="Arturo Laurenzi",
     author_email="arturo.laurenzi@iit.it",
     description="Package for generation of symbolic (casadi::SX) expressions of robot kinematics and dynamics. Based on URDF and Pinocchio.",
