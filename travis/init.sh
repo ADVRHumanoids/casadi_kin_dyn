18aa6b174477470bbda698bbe378d8000ec37dad
FrancescoRuscelli <francescoruscelli24@gmail.com>
2023-06-09 19:30:54 +0200

    missing boost dependency in travis

--- travis/init.sh
+++ travis/init.sh
@@ -4,9 +4,7 @@ set -ev
 echo "Travis tag is $TRAVIS_TAG"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt update && sudo apt install libgtest-dev libyaml-cpp-dev python3-pip python3-setuptools liburdfdom-dev cmake libeigen3-dev libboost-filesystem-dev libboost-serialization-dev libblas-dev liblapack-dev patchelf python3-venv python3-wheel python3-nose
-
-
+sudo apt update && sudo apt install python3-pip python3-setuptools liburdfdom-dev cmake libeigen3-dev libboost-all-dev libboost-filesystem-dev libboost-chrono-dev libboost-serialization-dev libblas-dev liblapack-dev patchelf python3-venv libassimp-dev
 if [ "$DISTRIBUTION" = "python" ]; then
     # prepare environment for python install
     ./travis/init_forest.sh
