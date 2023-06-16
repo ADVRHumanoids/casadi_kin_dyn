40554759949a4da21a6a0abd98cfbc138297b424
FrancescoRuscelli <francescoruscelli24@gmail.com>
2023-06-16 10:16:34 +0200

    exploring tests conda

--- conda/run_test.py
+++ conda/run_test.py
@@ -1,3 +1,8 @@
+import os
+import sys
+print(os.environ['CONDA_DEFAULT_ENV'])
+print(os.environ['PYTHONPATH'])
+
 from casadi_kin_dyn import pycasadi_kin_dyn as cas_kin_dyn
 
 urdf = open('cart_pole.urdf', 'r').read()
