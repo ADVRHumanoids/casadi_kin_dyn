bd67fd7e970840b1ab7badb451547e9a9ae639d3
FrancescoRuscelli <francescoruscelli24@gmail.com>
2023-06-16 18:59:52 +0200

    removed language python for pyenv mess

--- conda/run_test.py
+++ conda/run_test.py
@@ -1,18 +1,7 @@
 import os
 import sys
 
-import conda.gateways.logging
-from conda.cli.main_list import list_packages
-from conda.common.compat import text_type
-
-for elem in sys.path:
-
-    exitcode, output = list_packages(elem)
-    print('\n'.join(map(text_type, output)))
-
 print('sys.path:', sys.path)
-
-
 print(os.environ['CONDA_DEFAULT_ENV'])
 
 from casadi_kin_dyn import pycasadi_kin_dyn as cas_kin_dyn
