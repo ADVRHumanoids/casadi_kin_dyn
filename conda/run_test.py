3005b3687a7d06a5916bf7c64ac4f6615d7a2f68
FrancescoRuscelli <francescoruscelli24@gmail.com>
2023-06-20 12:40:21 +0200

    this is probably working

--- conda/run_test.py
+++ conda/run_test.py
@@ -1,9 +1,6 @@
 import os
 import sys
 
-print('sys.path:', sys.path)
-print(os.environ['CONDA_DEFAULT_ENV'])
-
 from casadi_kin_dyn import pycasadi_kin_dyn as cas_kin_dyn
 
 urdf = open('cart_pole.urdf', 'r').read()
