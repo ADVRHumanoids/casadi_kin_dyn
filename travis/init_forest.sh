14858d9dda4f4bd9d8d6de26b11d5d7bb758f5f9
FrancescoRuscelli <francescoruscelli24@gmail.com>
2023-11-20 14:20:15 +0100

    not working

--- travis/init_forest.sh
+++ travis/init_forest.sh
@@ -5,9 +5,9 @@ cd .. && mkdir forest_ws && cd forest_ws && forest init  # create forest ws for 
 # source setup.bash
 ln -s $SRC_FOLDER src/$(basename $SRC_FOLDER)  # symlink original source folder
 
-ls ../casadi_kin_dyn
+ls ../casadi_kin_dyn/travis/
 
-cp -R ../casadi_kin_dyn/recipes .
+cp -R ../casadi_kin_dyn/travis/recipes .
 
 # FOREST_TAG='master'
 # forest add-recipes git@github.com:advrhumanoids/multidof_recipes.git --tag $FOREST_TAG --clone-protocol https  # get recipes from repo
