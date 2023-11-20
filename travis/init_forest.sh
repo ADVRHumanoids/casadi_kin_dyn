1caecc1a9e1b7e471fbcb1a2aeab5e5b681f273b
FrancescoRuscelli <francescoruscelli24@gmail.com>
2023-11-20 11:25:12 +0100

    added recipes manually for version control instead of downloading from multidof_recipes

--- travis/init_forest.sh
+++ travis/init_forest.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -i
-pip3 install build twine hhcm-forest==1.1.11
+pip3 install build twine hhcm-forest==1.1.14
 SRC_FOLDER=$PWD
 cd .. && mkdir forest_ws && cd forest_ws && forest init  # create forest ws for building
 # source setup.bash
 ln -s $SRC_FOLDER src/$(basename $SRC_FOLDER)  # symlink original source folder
+
+ls .
+cp ../travis/recipes .
 FOREST_TAG='master'
 forest add-recipes git@github.com:advrhumanoids/multidof_recipes.git --tag $FOREST_TAG --clone-protocol https  # get recipes from repo
