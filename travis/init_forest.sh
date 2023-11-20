4b25a16148960a444d47676f0b541a8c2d5f6e8f
FrancescoRuscelli <francescoruscelli24@gmail.com>
2023-11-20 15:52:45 +0100

    move instead of cp

--- travis/init_forest.sh
+++ travis/init_forest.sh
@@ -5,10 +5,8 @@ cd .. && mkdir forest_ws && cd forest_ws && forest init  # create forest ws for 
 # source setup.bash
 ln -s $SRC_FOLDER src/$(basename $SRC_FOLDER)  # symlink original source folder
 
-ls ../casadi_kin_dyn/travis/
-
-echo -e "moving build recipes to $PWD"
-cp -R ../casadi_kin_dyn/travis/recipes .
+echo "moving build recipes to $PWD"
+mv ../casadi_kin_dyn/travis/recipes .
 
 # FOREST_TAG='master'
 # forest add-recipes git@github.com:advrhumanoids/multidof_recipes.git --tag $FOREST_TAG --clone-protocol https  # get recipes from repo
